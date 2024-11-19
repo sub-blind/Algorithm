656ba406b822edbbc0f636b46aa7ccc13461f3b0
Kim <58137602+sub-blind@users.noreply.github.com>
2024-11-19 21:31:00 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 284 ms, Memory: 121388 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,27 @@
+from sys import stdin
+
+n, m = map(int, stdin.readline().split())
+numbers = [[0] * (n + 1)]
+
+for _ in range(n):
+    nums = [0] + list(map(int, stdin.readline().split()))
+    numbers.append(nums)
+
+# 행별 누적 합
+for i in range(1, n + 1):
+    for j in range(1, n):
+        numbers[i][j + 1] += numbers[i][j]
+
+# 열별 누적 합
+for j in range(1, n + 1):
+    for i in range(1, n):
+        numbers[i + 1][j] += numbers[i][j]
+
+for _ in range(m):
+    x1, y1, x2, y2 = map(int, stdin.readline().split())
+    print(
+        numbers[x2][y2] 
+        - numbers[x1 - 1][y2] 
+        - numbers[x2][y1 - 1] 
+        + numbers[x1 - 1][y1 - 1]
+    )
