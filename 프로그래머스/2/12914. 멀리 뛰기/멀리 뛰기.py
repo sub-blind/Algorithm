eb92f68516ac25f53528425da83e6b97468e0469
Kim <58137602+sub-blind@users.noreply.github.com>
2025-05-06 17:47:20 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.20 ms, Memory: 9.24 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,12 @@
+def solution(n):
+    MOD = 1234567
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+        
+    a, b = 1, 2
+    for i in range(3, n + 1):
+        a, b = b, (a + b) % MOD
+    
+    return b
