9fcf9bf855779698699485fd45b0f8966dc4a509
Kim <58137602+sub-blind@users.noreply.github.com>
2025-08-03 16:49:41 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅌ, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/23811. 골뱅이 찍기 － ㅌ/골뱅이 찍기 － ㅌ.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+for i in range(N):
+    print('@'*(5*N))
+
+for i in range(N):
+    print('@'*N)
+
+for i in range(N):
+    print('@'*(5*N))
+
+for i in range(N):
+    print('@'*N)
+
+for i in range(N):
+    print('@'*(5*N))
