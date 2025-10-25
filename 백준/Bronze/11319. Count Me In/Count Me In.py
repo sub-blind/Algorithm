02a1497941fd2e0c9cfb782da1dcd16bf47d5b8f
Kim <58137602+sub-blind@users.noreply.github.com>
2025-10-25 23:13:13 +0900

    [Bronze III] Title: Count Me In, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/11319. Count Me In/Count Me In.py
@@ -0,0 +1,10 @@
+a = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u']
+for _ in range(int(input())):
+    s = input()
+    c = v = 0
+    for t in s:
+        if t in a:
+            v += 1
+        elif t.isalpha():
+             c += 1
+    print(c, v)
