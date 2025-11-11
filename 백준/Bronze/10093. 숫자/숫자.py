9c95e865971442abf7efa84ed27fdd5e97bd49b0
Kim <58137602+sub-blind@users.noreply.github.com>
2025-11-11 21:51:52 +0900

    [Bronze II] Title: 숫자, Time: 88 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.py
@@ -0,0 +1,12 @@
+a, b = map(int, input().split())
+
+if a > b:
+	print(a - b - 1)
+	for i in range(b + 1, a):
+		print(i, end=" ")
+elif a < b:
+	print(b - a - 1)
+	for i in range(a + 1, b):
+		print(i, end=" ")
+else:
+	print(a - b)
